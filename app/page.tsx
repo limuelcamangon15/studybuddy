c4b720e0f36e25e339568e0a7619274b5c8fb3de
Limuel Camangon <limuelmartincamangon@gmail.com>
2025-12-02 10:24:41 +0800

    haha

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <h1 className="text-2xl font-medium">Hellowwwwww idol</h1>
+      <h1 className="text-2xl font-medium">Hellowwwww BULSU!!!!!!!!</h1>
     </div>
   );
 }
