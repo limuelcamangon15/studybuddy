d1963bee4eefae910fc363ddabf790a201ebc568
Limuel <limuelmartincamangon@gmail.com>
2025-12-04 00:23:19 +0800

    fixed responsiveness and footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,28 @@
+import { BiLogoFacebook, BiLogoGithub } from "react-icons/bi";
+
 function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center p-2">
-      <h4>StudyBuddy © {new Date().getFullYear()}</h4>
+    <footer className="w-full flex flex-row gap-7 items-center justify-center p-2">
+      <p>StudyBuddy {new Date().getFullYear()} © Limuel Camangon </p>
+
+      <div className="flex gap-3">
+        <a
+          href="https://github.com/limuelcamangon15"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="links-logo"
+        >
+          <BiLogoGithub className="text-xl hover:text-gray-700 transition" />
+        </a>
+        <a
+          href="https://www.facebook.com/lims.1506"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="links-logo"
+        >
+          <BiLogoFacebook className="text-xl hover:text-gray-700 transition" />
+        </a>
+      </div>
     </footer>
   );
 }
